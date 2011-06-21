3c171160238e288f2c5fb6aa140bf6e4323740ff
tmarkiewicz <tmarkiewicz@gmail.com>
2011-06-21 11:48:43 -0600

    refactored Net::HTTP connection

--- lib/statsmix.rb
+++ lib/statsmix.rb
@@ -3,6 +3,7 @@ require 'net/http'
 class StatsMix
   
   BASE_URI = 'http://www.statsmix.com/api/v2/'
+  # BASE_URI = 'http://localhost:3000/api/v2/'
   
   GEM_VERSION = File.exist?('../VERSION') ? File.read('../VERSION') : ""
   
@@ -12,17 +13,22 @@ class StatsMix
     @user_agent = "StatsMix Ruby Gem " + GEM_VERSION
   end
   
-  # -- Stats --
+  def connect(resource)
+    # Resources available: stats, metrics, TODO: profiles
+    @url = URI.parse(BASE_URI + resource)
+    @connection = Net::HTTP.new(@url.host, @url.port)
+    
+  end
+  
+  # Stats
   
   # List stats (index)
   # 
   # Required: metric_id
   # Optional: limit
   def list_stats(metric_id, limit = nil)
-    url = URI.parse(BASE_URI + "stats")
-    http = Net::HTTP.new(url.host, url.port)
-    
-    request = Net::HTTP::Get.new(url.path + '.' + @format)
+    connect('stats')
+    request = Net::HTTP::Get.new(@url.path + '.' + @format)
     request["User-Agent"] = @user_agent
     
     form_hash = Hash.new
@@ -34,7 +40,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -43,15 +49,13 @@ class StatsMix
   # Required: stat_id
   # Optional: none
   def get_stat(stat_id)
-    url = URI.parse(BASE_URI + "stats")
-    http = Net::HTTP.new(url.host, url.port)
-    
-    request = Net::HTTP::Get.new(url.path + '/' + stat_id.to_s + '.' + @format)
+    connect('stats')
+    request = Net::HTTP::Get.new(@url.path + '/' + stat_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
     
     request.set_form_data({"api_key" => @api_key})
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -60,10 +64,8 @@ class StatsMix
   # Required: metric_id
   # Optional: value, generated_at, meta
   def create_stat(metric_id, value = 1, generated_at = Time.now, meta = nil)
-    url = URI.parse(BASE_URI + "stats")
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Post.new(url.path + '.' + @format)
+    connect('stats')
+    request = Net::HTTP::Post.new(@url.path + '.' + @format)
     request["User-Agent"] = @user_agent
 
     form_hash = Hash.new
@@ -77,7 +79,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -86,10 +88,8 @@ class StatsMix
   # Required: stat_id
   # Optional: value, generated_at, meta
   def update_stat(stat_id, value = nil, generated_at = nil, meta = nil)  
-    url = URI.parse(BASE_URI + "stats")
-    http = Net::HTTP.new(url.host, url.port)
-  
-    request = Net::HTTP::Put.new(url.path + '/' + stat_id.to_s + '.' + @format)
+    connect('stats')
+    request = Net::HTTP::Put.new(@url.path + '/' + stat_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
 
     form_hash = Hash.new
@@ -106,7 +106,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
 
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -115,29 +115,25 @@ class StatsMix
   # Required: stat_id
   # Optional: none
   def delete_stat(stat_id)    
-    url = URI.parse(BASE_URI + "stats")
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Delete.new(url.path + '/' + stat_id.to_s + '.' + @format)
+    connect('stats')
+    request = Net::HTTP::Delete.new(@url.path + '/' + stat_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
     
     request.set_form_data({"api_key" => @api_key})
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
-  # -- Metrics --
+  # Metrics
   
   # List metrics
   # 
   # Required: none
   # Optional: profile_id, limit
   def list_metrics(profile_id = nil, limit = nil)
-    url = URI.parse(BASE_URI + "metrics")
-    http = Net::HTTP.new(url.host, url.port)
-    
-    request = Net::HTTP::Get.new(url.path + '.' + @format)
+    connect('metrics')
+    request = Net::HTTP::Get.new(@url.path + '.' + @format)
     request["User-Agent"] = @user_agent
     
     form_hash = Hash.new
@@ -151,7 +147,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -160,15 +156,13 @@ class StatsMix
   # Required: metric_id
   # Optional: none
   def get_metric(metric_id)
-    url = URI.parse(BASE_URI + "metrics")
-    http = Net::HTTP.new(url.host, url.port)
-    
-    request = Net::HTTP::Get.new(url.path + '/' + metric_id.to_s + '.' + @format)
+    connect('metrics')
+    request = Net::HTTP::Get.new(@url.path + '/' + metric_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
     
     request.set_form_data({"api_key" => @api_key})
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -177,10 +171,8 @@ class StatsMix
   # Required: name
   # Optional: profile_id, generated_at
   def create_metric(name, profile_id = nil)
-    url = URI.parse(BASE_URI + "metrics")
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Post.new(url.path + '.' + @format)
+    connect('metrics')
+    request = Net::HTTP::Post.new(@url.path + '.' + @format)
     request["User-Agent"] = @user_agent
 
     form_hash = Hash.new
@@ -192,7 +184,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -201,10 +193,8 @@ class StatsMix
   # Required: metric_id
   # Optional: name, sharing, include_in_email
   def update_metric(metric_id, name = nil, sharing = nil, include_in_email = nil)  
-    url = URI.parse(BASE_URI + "metrics")
-    http = Net::HTTP.new(url.host, url.port)
-  
-    request = Net::HTTP::Put.new(url.path + '/' + metric_id.to_s + '.' + @format)
+    connect('metrics')
+    request = Net::HTTP::Put.new(@url.path + '/' + metric_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
 
     form_hash = Hash.new
@@ -221,7 +211,7 @@ class StatsMix
     
     request.set_form_data(form_hash)
 
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
   
@@ -230,15 +220,13 @@ class StatsMix
   # Required: metric_id
   # Optional: none
   def delete_metric(metric_id)    
-    url = URI.parse(BASE_URI + "metrics")
-    http = Net::HTTP.new(url.host, url.port)
-
-    request = Net::HTTP::Delete.new(url.path + '/' + metric_id.to_s + '.' + @format)
+    connect('metrics')
+    request = Net::HTTP::Delete.new(@url.path + '/' + metric_id.to_s + '.' + @format)
     request["User-Agent"] = @user_agent
     
     request.set_form_data({"api_key" => @api_key})
     
-    response = http.request(request)
+    response = @connection.request(request)
     puts response.body, response.code, response.message
   end
 end
