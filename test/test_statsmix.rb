8b2e19f2e2d8d2318c4ebde71c1fa8ca7cab8f56
tmarkiewicz <tmarkiewicz@gmail.com>
2011-06-21 11:31:51 -0600

    updated tests, readme

--- test/test_statsmix.rb
+++ test/test_statsmix.rb
@@ -1,11 +1,60 @@
 require 'helper'
 require 'statsmix'
 
-class TestStatsmix < Test::Unit::TestCase  
+class TestStatsmix < Test::Unit::TestCase
+  
+  # TODO use fakwweb gem for testing
+  # http://technicalpickles.com/posts/stop-net-http-dead-in-its-tracks-with-fakeweb/
+  # https://github.com/chrisk/fakeweb
+  # http://fakeweb.rubyforge.org/
+  
+  # TODO use VCR for tests
+  # http://www.rubyinside.com/vcr-a-recorder-for-all-your-tests-http-interactions-4169.html
+  # https://github.com/myronmarston/vcr
+  
   should "initialize StatsMix API" do
-    # TODO setup a test account to be used for live testing of API
-    
-    # statsmix = StatsMix.new('api_key', 'xml')
-    # statsmix.list_metrics(11, 3)
+    # statsmix = StatsMix.new('59f08613db2691f28afe', 'xml')
   end
+  
+  # # metrics
+  # should "create metric" do
+  #   
+  # end
+  # 
+  # should "get metric" do
+  # 
+  # end
+  # 
+  # should "list metrics" do
+  #   
+  # end
+  # 
+  # should "update metric" do
+  #   
+  # end
+  # 
+  # should "delete metric" do
+  #   
+  # end
+  # 
+  # # stats
+  # should "create stat" do
+  #   
+  # end
+  # 
+  # should "get stat" do
+  #   
+  # end
+  # 
+  # should "list stats" do
+  #   
+  # end
+  # 
+  # should "update stat" do
+  #   
+  # end
+  # 
+  # should "delete stat" do
+  #   
+  # end
 end
