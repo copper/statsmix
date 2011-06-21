a31eed0fd78be20d6827f51bcb6dd37ce3c463ab
tmarkiewicz <tmarkiewicz@gmail.com>
2011-06-20 18:32:08 -0600

    added todos in tests

--- test/test_statsmix.rb
+++ test/test_statsmix.rb
@@ -3,6 +3,8 @@ require 'statsmix'
 
 class TestStatsmix < Test::Unit::TestCase  
   should "initialize StatsMix API" do
+    # TODO setup a test account to be used for live testing of API
+    
     # statsmix = StatsMix.new('api_key', 'xml')
     # statsmix.list_metrics(11, 3)
   end
