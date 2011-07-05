b43ea2c1655c6d68861a016e1d6f78ec3bd2ecb7
Derek Scruggs <me@derekscruggs.com>
2011-07-05 11:00:14 -0600

    Accidentally committed base uri from local test. Changed uri back to production

--- lib/statsmix.rb
+++ lib/statsmix.rb
@@ -4,7 +4,7 @@ require 'json'
 
 class StatsMix
   
-  BASE_URI = 'http://localhost:3000/api/v2/'
+  BASE_URI = 'https://statsmix.com/api/v2/'
   
   GEM_VERSION = File.exist?('../VERSION') ? File.read('../VERSION') : ""
 
