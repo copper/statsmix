3f797213a62f1563ff835b8830cec5aa3b59c846
Derek Scruggs <me@derekscruggs.com>
2011-07-06 13:37:21 -0600

    Added support for start_date and end_date in list_stats (third param accepts a hash)

--- lib/statsmix.rb
+++ lib/statsmix.rb
@@ -293,14 +293,4 @@ class StatsMix
   end
 end
 
-#added to suppress ssl warnings per advice at http://www.5dollarwhitebox.org/drupal/node/64
-# class Net::HTTP
-#   alias_method :old_initialize, :initialize
-#   def initialize(*args)
-#     old_initialize(*args)
-#     @ssl_context = OpenSSL::SSL::SSLContext.new
-#     @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-#   end
-# end
-
 StatsMix.api_from_env
