a66b327f34d242231470b970ebdab1ae3d216866
tmarkiewicz <tmarkiewicz@gmail.com>
2011-12-06 10:27:08 -0700

    updates and comments to testing shell script

--- test_all_rubies.sh
+++ test_all_rubies.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
+#  inspired by http://pivotallabs.com/users/jdean/blog/articles/1728-testing-your-gem-against-multiple-rubies-and-rails-versions-with-rvm
+#  on OS X, run using this command: sh test_all_rubies.sh
+
 function run {
   gem list --local bundler | grep bundler || gem install bundler --no-ri --no-rdoc
-
-  # echo 'Running bundle exec rspec spec against activesupport / activerecord 2.3.2...'
-  # ACTIVE_HASH_ACTIVERECORD_VERSION=2.3.2 bundle update activerecord
-  # bundle exec rspec spec
-
   echo 'Running tests...'
   rake test
 }
 
-# rvm use ruby-1.8.7@active_hash --create
-# run
+rvm use ruby-1.8.6
+run
 
 rvm use ruby-1.8.7
+run
+
+rvm use ruby-1.9.1
 run
 
 rvm use ruby-1.9.2
