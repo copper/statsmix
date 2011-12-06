f14a59e958770fade8bfbaf818d3be513f4508c1
tmarkiewicz <tmarkiewicz@gmail.com>
2011-12-06 10:39:29 -0700

    more updates to testing shell script

--- test_all_rubies.sh
+++ test_all_rubies.sh
@@ -3,6 +3,9 @@
 #  inspired by http://pivotallabs.com/users/jdean/blog/articles/1728-testing-your-gem-against-multiple-rubies-and-rails-versions-with-rvm
 #  on OS X, run using this command: sh test_all_rubies.sh
 
+# tells the shell to exit immediately if any of the commands in the script fail
+set -e
+
 function run {
   gem list --local bundler | grep bundler || gem install bundler --no-ri --no-rdoc
   echo 'Running tests...'
@@ -15,10 +18,16 @@ run
 rvm use ruby-1.8.7
 run
 
+# rvm use ree-1.8.7
+# run
+
 rvm use ruby-1.9.1
 run
 
 rvm use ruby-1.9.2
+run
+
+rvm use ruby-1.9.3
 run
 
 echo 'Success!'
